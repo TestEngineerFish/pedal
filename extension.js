21c307e3c50bdb43bed24488df37867b3dbc2431
sam <916878440@qq.com>
2023-10-26 16:59:44 +0800

    增加项目内缓存清除逻辑

--- extension.js
+++ extension.js
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 const { error } = require('console');
 const vscode = require('vscode');
+const fs = require('fs');
+const { exec } = require('child_process');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -15,45 +17,13 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "pedal" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('pedal.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from pedal!');
-	});
-
-	const hover = vscode.languages.registerHoverProvider('json', {
-		provideHover(document, position, token) {
-			const fileName = document.fileName;
-			const word = document.getText(document.getWordRangeAtPosition(position));
-			return new vscode.Hover("悬停测试");
-		}
-	})
 	let cleanCache = vscode.commands.registerCommand('pedal.cleanCache', function () {
-		const rootPath = vscode.workspace.getConfiguration("pedal").get('customPath');
-		const fs = require('fs');
-		fs.readdir(rootPath, (err, files) => {
-			if (err) {
-				vscode.window.showInformationMessage('获取{path}下文件失败 ' + error);
-			} else {
-				files.forEach( file => {
-					const filePath = '${rootPath}/${file}';
-					fs.unlink(filePath, err => {
-						if (err) {
-							vscode.window.showInformationMessage('删除文件${file}失败 ' + error);
-						} else {
-							vscode.window.showInformationMessage('删除文件${file}成功 ' + error);
-						}
-					});
-				});
-			}
-		});
+		var _terminal = vscode.window.createTerminal({name: "pedal",location:vscode.TerminalLocation.Editor});
+		_terminal.show(false);
+		deleteFlutterCache(_terminal);
 	});
 
-	context.subscriptions.push(disposable, hover, cleanCache);
+	context.subscriptions.push(cleanCache);
 }
 
 // This method is called when your extension is deactivated
@@ -62,4 +32,78 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
+}
+
+// MARK: ==== Event ====
+function deleteFlutterCache(_terminal) {
+	if (vscode.workspace.workspaceFolders.length == 0) { return; }
+	const projectRootPath = vscode.workspace.workspaceFolders[0].uri.path;
+	const projectPubspecLockFile = `${projectRootPath}/pubspec.lock`;
+	const examplePubspacLockFile = `${projectRootPath}/example/pubspec.lock`;
+
+	_terminal.sendText(`cd ${projectRootPath}`);
+
+	fs.access(projectPubspecLockFile, fs.constants.F_OK, (err) => {
+		if (!err) {
+			_terminal.sendText(`rm -rf ${projectPubspecLockFile}`);
+		}
+	});
+	fs.access(examplePubspacLockFile, fs.constants.F_OK, (err) => {
+		if (!err) {
+			_terminal.sendText(`rm -rf ${examplePubspacLockFile}`);
+		}
+	});
+
+	const cacheRootPath = vscode.workspace.getConfiguration("pedal").get('flutterCacheRootPath');
+	if (!cacheRootPath.includes('.pub-cache')) { 
+		vscode.window.showInformationMessage('获取Flutter缓存根目录地址失败 ❌');
+		return;
+	} else {
+		_terminal.sendText(`rm -rf ${cacheRootPath}`);
+		
+		const projectPubspecPath = `${projectRootPath}/pubspec.yaml`;
+		// const examplePubspacPath = `${projectRootPath}/example/pubspec.yaml`;
+		
+		fs.access(projectPubspecPath, fs.constants.F_OK, (err) => {
+			if (!err) {
+				_terminal.sendText(`flutter pub get`);
+			}
+			deletePodsCache(_terminal);
+		});
+	}
+}
+
+function deletePodsCache(_terminal) {
+
+	// project cache 
+	if (vscode.workspace.workspaceFolders.length == 0) { return; }
+	const projectRootPath = vscode.workspace.workspaceFolders[0].uri.path;
+	var projectIOSPath = `${projectRootPath}/example/ios`;
+	
+	if (!fs.existsSync(projectIOSPath)) {
+		projectIOSPath = `${projectRootPath}/ios`;
+	}
+	
+	const podsLockFile = `${projectIOSPath}/Podfile.lock`
+	const podsPath = `${projectIOSPath}/Pods`
+	fs.access(podsLockFile, fs.constants.F_OK, (err) => {
+		if (err) {
+			_terminal.sendText(`rm -rf ${podsLockFile}`);
+		}
+	});
+	if (fs.existsSync(podsPath)) {
+		_terminal.sendText(`rm -rf ${podsLockFile}`);
+	}
+	// system cache
+	const rootPath = vscode.workspace.getConfiguration("pedal").get('podCacheRootPath');
+	if (!rootPath.includes('CocoaPods')) { 
+		vscode.window.showInformationMessage('获取Cocoapods缓存根目录地址失败 ❌');
+	} else {
+		if (fs.existsSync(rootPath)) {
+			_terminal.sendText(`rm -rf ${rootPath}`);
+		} 
+		// get new data
+		_terminal.sendText(`cd ${projectIOSPath}`);
+		_terminal.sendText(`pod install`);
+	}
 }
